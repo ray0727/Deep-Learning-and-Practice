9fca56e7498a539a4939be09024cf65e56aed69c
ray0727 <rayhuang100@gmail.com>
2021-04-24 22:40:32 +0800

    EEGnet finish

--- HW3/EEGNet.py
+++ HW3/EEGNet.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 import torch.optim as optim
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
@@ -40,42 +41,56 @@ class EEGNet(nn.Module):
         x = self.classify(x)
         return x
 
-def train(train_loader, test_loader, activation, device):
+def train(train_loader, test_loader, activations, device):
     criterion = nn.CrossEntropyLoss()
-    learning_rate = 0.001
-    epochs = 300
-    model = EEGNet(activation)
-    model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    learning_rate = 0.0005
+    epochs = 350
+    df = pd.DataFrame()
+    df['epoch'] = range(1, epochs+1)
+    best_test_acc = {"ReLU":0, "LeakyReLU":0, "ELU":0}
+    for name, activation in activations.items():
+        model = EEGNet(activation)
+        model.to(device)
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        acc_train = list()
+        acc_test = list()
+        print("Training with activation:", name)
+        for epoch in range(1, epochs+1):
+            model.train()
+            correct = 0
+            total_loss = 0
+            for _, (inputs, label) in enumerate(train_loader):
+                inputs = inputs.to(device, dtype=torch.float)
+                label = label.to(device, dtype=torch.long)
+                predict = model(inputs)
+                loss = criterion(predict, label)
+                total_loss += loss.item()
+                
+                pred = predict.argmax(dim=1)
+                for i in range(len(label)):
+                    if pred[i] == label[i]:
+                        correct +=1
 
-    for epoch in range(1, epochs+1):
-        model.train()
-        correct = 0
-        total_correct = 0
-        total_loss = 0
-        for _, (inputs, label) in enumerate(train_loader):
-            inputs = inputs.to(device, dtype=torch.float)
-            label = label.to(device, dtype=torch.long)
-            predict = model(inputs)
-            loss = criterion(predict, label)
-            total_loss += loss.item()
-            
-            pred = predict.argmax(dim=1)
-            for i in range(len(label)):
-                if pred[i] == label[i]:
-                    correct +=1
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-        total_loss = total_loss / float(len(train_loader.dataset))
-        total_correct = 100. * correct / float(len(train_loader.dataset))
-        # print(total_correct)
-        if epoch%10==0:
-            print(f'epoch{epoch:>3d}  loss:{total_loss:.4f}  acc:{total_correct:.2f}%')
-        model.eval()
-        correct_test = evaluate(model, test_loader, device)
-        if epoch%10==0:
-            print(f'epoch{epoch:>3d}  acc_test:{correct_test:.2f}%')
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            total_loss = total_loss / float(len(train_loader.dataset))
+            correct = 100. * correct / float(len(train_loader.dataset))
+            acc_train.append(correct)
+            if epoch%10==0:
+                print(f'epoch{epoch:>3d}  loss:{total_loss:.4f}  acc:{correct:.3f}%')
+            model.eval()
+            correct_test = evaluate(model, test_loader, device)
+            if correct_test > best_test_acc[name]:
+                best_test_acc[name] = correct_test
+            acc_test.append(correct_test)
+            if epoch%10==0:
+                print(f'epoch{epoch:>3d}  acc_test:{correct_test:.2f}%')
+
+        df[name+"_train"] = acc_train
+        df[name+"_test" ] = acc_test
+    return df, best_test_acc
 
 def evaluate(model, test_loader, device):
     model.eval()
@@ -92,6 +107,15 @@ def evaluate(model, test_loader, device):
     correct = 100. * correct / float(len(test_loader.dataset))
     return correct
 
+def plot(dataframe):
+    plt.figure(figsize=(10,5))
+    plt.title("Activation function comparision(EEGNet)", fontsize=18)
+    plt.ylabel("Accuracy(%)", fontsize=18)
+    plt.xlabel("Epoch", fontsize=18)
+    for name in dataframe.columns[1:]:
+        plt.plot('epoch', name, data=dataframe)
+    plt.legend(loc="lower right")
+    plt.show()
 
 class DeepConvNet(nn.Module):
     def __init__(self, activation=nn.ReLU):
@@ -124,12 +148,16 @@ if __name__== "__main__":
     print(device)
     train_data, train_label, test_data, test_label = read_bci_data()
     dataset = TensorDataset(torch.from_numpy(train_data), torch.from_numpy(train_label))
-    train_dataset = DataLoader(dataset, batch_size=256, shuffle=True)
-
-    dataset = TensorDataset(torch.from_numpy(test_data), torch.from_numpy(test_label))
-    test_dataset = DataLoader(dataset, batch_size=256, shuffle=True)
-    train(train_dataset, test_dataset, nn.ReLU(), device)
-
+    train_dataset = DataLoader(dataset, batch_size=64, shuffle=True)
     # for _, (inputs, label) in enumerate(train_dataset):
-    #         print("label:", label)
+    #     print("label:", label)
+    dataset = TensorDataset(torch.from_numpy(test_data), torch.from_numpy(test_label))
+    test_dataset = DataLoader(dataset, batch_size=64, shuffle=True)
+    
+    activations = {"ReLU":nn.ReLU(), "LeakyReLU":nn.LeakyReLU(), "ELU":nn.ELU()}
+    data, best_test_acc = train(train_dataset, test_dataset, activations, device)
+    plot(data)
+    for name, acc in best_test_acc.items():
+        print(name, " best accuracy: ", acc, "%")
+
     
