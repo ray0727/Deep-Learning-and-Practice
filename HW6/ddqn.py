935c9da0976bfce76f643d01119f925ebdbb50d4
ray0727 <rayhuang100@gmail.com>
2021-05-24 20:19:48 +0800

    HW6 done

---
+++ HW6/ddqn.py
@@ -0,0 +1,247 @@
+'''DLP DDQN Lab'''
+__author__ = 'chengscott'
+__copyright__ = 'Copyright 2020, NCTU CGI Lab'
+import argparse
+from collections import deque
+import itertools
+import random
+import time
+
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+
+
+class ReplayMemory:
+    __slots__ = ['buffer']
+
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def append(self, *transition):
+        # (state, action, reward, next_state, done)
+        self.buffer.append(tuple(map(tuple, transition)))
+
+    def sample(self, batch_size, device):
+        '''sample a batch of transition tensors'''
+        transitions = random.sample(self.buffer, batch_size)
+        return (torch.tensor(x, dtype=torch.float, device=device)
+                for x in zip(*transitions))
+
+
+class Net(nn.Module):
+    def __init__(self, state_dim=8, action_dim=4, hidden_dim=(300,300)):
+        super().__init__()
+        ## TODO ##
+        self.fc1=nn.Linear(state_dim,hidden_dim[0])
+        self.fc2=nn.Linear(hidden_dim[0],hidden_dim[1])
+        self.fc3=nn.Linear(hidden_dim[1],action_dim)
+        self.relu=nn.ReLU()
+
+    def forward(self, x):
+        ## TODO ##
+        x=self.relu(self.fc1(x))
+        x=self.relu(self.fc2(x))
+        x=self.fc3(x)
+        return x
+
+class DDQN:
+    def __init__(self, args):
+        self._behavior_net = Net().to(args.device)
+        self._target_net = Net().to(args.device)
+        # initialize target network
+        self._target_net.load_state_dict(self._behavior_net.state_dict())
+        ## TODO ##
+        self._optimizer = optim.Adam(self._behavior_net.parameters(),lr=args.lr)
+
+        # memory
+        self._memory = ReplayMemory(capacity=args.capacity)
+
+        ## config ##
+        self.device = args.device
+        self.batch_size = args.batch_size
+        self.gamma = args.gamma
+        self.freq = args.freq   # update behavior network every 4 iterations
+        self.target_freq = args.target_freq    # update behavior network every 1000 iterations
+
+    def select_action(self, state, epsilon, action_space):
+        '''epsilon-greedy based on behavior network'''
+         ## TODO ##
+        if random.random() < epsilon:  # explore
+            return action_space.sample()
+        else: # exploit
+            with torch.no_grad():
+                # t.max(1) will return largest column value of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                return self._behavior_net(torch.from_numpy(state).view(1,-1).to(self.device)).max(dim=1)[1].item()
+
+
+    def append(self, state, action, reward, next_state, done):
+        self._memory.append(state, [action], [reward / 10], next_state,
+                            [int(done)])
+
+    def update(self, total_steps):
+        if total_steps % self.freq == 0:
+            self._update_behavior_network(self.gamma)
+        if total_steps % self.target_freq == 0:
+            self._update_target_network()
+
+    def _update_behavior_network(self, gamma):
+        # sample a minibatch of transitions
+        state, action, reward, next_state, done = self._memory.sample(self.batch_size, self.device)
+        ## TODO ##
+        q_value = self._behavior_net(state).gather(dim=1,index=action.long())
+        with torch.no_grad():
+            action_index=self._behavior_net(next_state).max(dim=1)[1].view(-1,1)
+            q_next = self._target_net(next_state).gather(dim=1,index=action_index.long())
+            q_target = reward + gamma*q_next*(1-done)
+        criterion = nn.MSELoss()
+        loss = criterion(q_value, q_target)
+
+        # bp
+        self._optimizer.zero_grad()
+        loss.backward()
+        nn.utils.clip_grad_norm_(self._behavior_net.parameters(), 5)
+        self._optimizer.step()
+
+    def _update_target_network(self):
+        '''update target network by copying from behavior network'''
+        ## TODO ##
+        self._target_net.load_state_dict(self._behavior_net.state_dict())
+
+
+    def save(self, model_path, checkpoint=False):
+        if checkpoint:
+            torch.save(
+                {
+                    'behavior_net': self._behavior_net.state_dict(),
+                    'target_net': self._target_net.state_dict(),
+                    'optimizer': self._optimizer.state_dict(),
+                }, model_path)
+        else:
+            torch.save({
+                'behavior_net': self._behavior_net.state_dict(),
+            }, model_path)
+
+    def load(self, model_path, checkpoint=False):
+        model = torch.load(model_path)
+        self._behavior_net.load_state_dict(model['behavior_net'])
+        if checkpoint:
+            self._target_net.load_state_dict(model['target_net'])
+            self._optimizer.load_state_dict(model['optimizer'])
+
+
+def train(args, env, agent, writer):
+    print('Start Training')
+    action_space = env.action_space
+    total_steps, epsilon = 0, 1.
+    ewma_reward = 0
+    for episode in range(args.episode):
+        total_reward = 0
+        state = env.reset()
+        epsilon = max(epsilon * args.eps_decay, args.eps_min)
+        for t in itertools.count(start=1):  # play an episode
+            # select action
+            if total_steps < args.warmup:
+                action = action_space.sample()
+            else:
+                action = agent.select_action(state, epsilon, action_space)
+            # execute action
+            next_state, reward, done, _ = env.step(action)
+
+            # store transition
+            agent.append(state, action, reward, next_state, done)
+
+            # update
+            if total_steps >= args.warmup:
+                agent.update(total_steps)
+
+            state = next_state
+            total_reward += reward
+            total_steps += 1
+            if done:
+                ewma_reward = 0.05 * total_reward + (1 - 0.05) * ewma_reward
+                writer.add_scalar('Train/Episode Reward', total_reward, total_steps)
+                writer.add_scalar('Train/Ewma Reward', ewma_reward, total_steps)
+                print(f'Step: {total_steps}\tEpisode: {episode}\tLength: {t:3d}\tTotal reward: {total_reward:.2f}\tEwma reward: {ewma_reward:.2f}\tEpsilon: {epsilon:.3f}')
+                break
+    env.close()
+
+
+def test(args, env, agent, writer):
+    print('Start Testing')
+    action_space = env.action_space
+    epsilon = args.test_epsilon
+    seeds = (args.seed + i for i in range(10))
+    rewards = []
+    for n_episode, seed in enumerate(seeds):
+        total_reward = 0
+        env.seed(seed)
+        state = env.reset()
+        ## TODO ##
+        for t in itertools.count(start=1):  # play an episode
+            env.render()
+            # select action
+            action = agent.select_action(state, epsilon, action_space)
+            # execute action
+            next_state, reward, done, _ = env.step(action)
+
+            state = next_state
+            total_reward += reward
+
+            if done:
+                writer.add_scalar('Test/Episode Reward', total_reward, n_episode)
+                print(f'total reward: {total_reward:.2f}')
+                rewards.append(total_reward)
+                break
+
+    print('Average Reward', np.mean(rewards))
+    env.close()
+
+
+def main():
+    ## arguments ##
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('-d', '--device', default='cuda')
+    parser.add_argument('-m', '--model', default='ddqn.pth')      # model path
+    parser.add_argument('--logdir', default='log/ddqn')
+    # train
+    parser.add_argument('--warmup', default=10000, type=int)
+    parser.add_argument('--episode', default=2000, type=int)#1200
+    parser.add_argument('--capacity', default=10000, type=int)
+    parser.add_argument('--batch_size', default=128, type=int)
+    parser.add_argument('--lr', default=.0005, type=float)
+    parser.add_argument('--eps_decay', default=.995, type=float)
+    parser.add_argument('--eps_min', default=.01, type=float)    # eps from 1.0 ~ 0.01
+    parser.add_argument('--gamma', default=.99, type=float)
+    parser.add_argument('--freq', default=4, type=int)           # update behavior network every 4 iterations
+    parser.add_argument('--target_freq', default=1000, type=int) # update target network every 1000 iterations
+    # test
+    parser.add_argument('--test_only', action='store_true')
+    parser.add_argument('--render', action='store_true')
+    parser.add_argument('--seed', default=20200519, type=int)
+    parser.add_argument('--test_epsilon', default=.001, type=float)
+    args = parser.parse_args()
+
+    ## main ##
+    env = gym.make('LunarLander-v2')
+    agent = DDQN(args)
+    writer = SummaryWriter(args.logdir)
+
+    if not args.test_only:
+        train(args, env, agent, writer)
+        agent.save(args.model,checkpoint=True)
+
+    agent.load(args.model,checkpoint=True)
+    test(args, env, agent, writer)
+
+
+if __name__ == '__main__':
+    main()
